5b8c550e2e6afc3ab41646b877cc3c4304c821f2
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-27 18:16:10 +0100

    Add CI and Release workflows, configure CMake, and implement module structure for dmell

---
+++ include/dmell.h
@@ -0,0 +1,6 @@
+#ifndef DMELL_H
+#define DMELL_H
+
+#include <dmod.h>
+
+#endif // DMELL_H
---
+++ src/dmell.c
@@ -0,0 +1,13 @@
+#include "dmell.h"
+
+/**
+ * @brief Main entry point of the module.
+ * 
+ * @param argc Number of arguments
+ * @param argv Array of arguments
+ * @return int Exit code
+ */
+int main(int argc, char** argv)
+{
+    return 0;
+}
